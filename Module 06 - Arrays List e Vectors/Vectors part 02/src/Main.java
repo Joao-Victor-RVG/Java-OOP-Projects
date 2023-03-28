ffec9e3a39cefbcca069fe60ef6cf3e9a671675b
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2023-03-28 17:53:34 -0300

    Vectors

---
+++ Module 06 - Arrays List e Vectors/Vectors part 02/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        int n = sc.nextInt();
+        Prodcuts[] vect = new Prodcuts[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Prodcuts(name, price);
+
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+        System.out.printf("AVEREGE PRICE = %.2fn", avg);
+
+
+        sc.close();
+    }
+}
